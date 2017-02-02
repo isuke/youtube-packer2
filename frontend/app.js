82c77d3e1218083c19a7eaa3fdf6695c17b63826
isuke <isuke770@gmail.com>
2017-02-03 00:12:02 +0900

    Use vuex

--- frontend/app.js
+++ frontend/app.js
@@ -1,10 +1,11 @@
 $ = require('jquery');
 Vue = require('vue');
+Vuex = require('vuex');
 Toastr = require('toastr');
-Store = require('store');
+LocalStorage = require('store');
 
 require("font-awesome-webpack");
 require("../node_modules/picnic/picnic.min.css");
 require("../node_modules/toastr/toastr.scss");
 
-require('./app.vue');
+require('./App.vue');
